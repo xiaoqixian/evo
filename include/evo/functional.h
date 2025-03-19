918d4c62989de3e377686a7b0604c47bbecff1a8
lunar <lunar_ubuntu@qq.com>
2025-03-19 22:50:25 +0800

    renamed all header files

--- include/evo/functional.h
+++
@@ -1,9 +0,0 @@
-// Date: Thu Nov 23 17:51:50 2023
-// Mail: lunar_ubuntu@qq.com
-// Author: https://github.com/xiaoqixian
-
-#ifndef _FUNCTIONAL_H
-#define _FUNCTIONAL_H
-
-
-#endif // _FUNCTIONAL_H
